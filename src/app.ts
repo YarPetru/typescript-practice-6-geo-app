84b4a3157a17a73d7d351aabf8b29950f81782ae
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2022-12-25 14:02:46 +0200

    finished wo updating packages

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,44 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_KEY = "AIzaSyA7yqnaRB2Ssgmgpcwe6jm7fyK59El_9p4";
+// const GOOGLE_KEY = "AIzaSyAw8_6Kpmc1omZNphMzSvKUlHcxsn7Kquk";
+
+// declare var google: any;
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+  const enteredAddress = addressInput.value;
+  console.log(encodeURI(enteredAddress));
+
+  type GoogleGeoResponse = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS";
+  };
+
+  axios
+    .get<GoogleGeoResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Unfortunately could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch((e) => console.error(e));
+}
+
+form.addEventListener("submit", searchAddressHandler);
